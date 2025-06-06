d4861ca6e90a36ea28ad673c111721e110f029b8
Chris Legg <c.legg@theclubcompany.com>
2025-06-06 17:23:32 +0100

    Create basic entities for the project

---
+++ app/Entity/Receipt.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity, Table(name: 'receipts')]
+class Receipt
+{
+    #[Id, Column(options: ['unsigned' => true]), GeneratedValue]
+    private int $id;
+
+    #[Column]
+    private string $fileName;
+
+    #[Column(name: 'created_at')]
+    private \DateTime $createdAt;
+
+    #[ManyToOne(inversedBy: 'receipts')]
+    private Transaction $transaction;
+
+}
