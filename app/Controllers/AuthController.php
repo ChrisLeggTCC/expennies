7c5140e0b1f3d15889265bbef5d4ae1ab8462386
Chris Legg <c.legg@theclubcompany.com>
2025-06-10 18:20:06 +0100

    Refactor to create ValidationException

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Entity\User;
+use App\Exception\ValidationException;
 use Doctrine\ORM\EntityManager;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-
 use Slim\Views\Twig;
+use Valitron\Validator;
 
 class  AuthController
 {
@@ -17,6 +18,7 @@ class  AuthController
     {
 
     }
+
     public function loginView(Request $request, Response $response): Response
     {
         return $this->twig->render($response, 'auth/login.twig');
@@ -30,11 +32,32 @@ class  AuthController
     public function register(Request $request, Response $response): Response
     {
         $data = $request->getParsedBody();
+
+        $v = new Validator($data);
+        $v->rule('required', ['name', 'email', 'password', 'confirmPassword']);
+        $v->rule('email', 'email');
+        $v->rule('equals', 'confirmPassword', 'password');
+        $v->rule(
+            fn($field, $value, $params, $fields) => !$this->entityManager->getRepository(User::class)->count(
+                ['email' => $value]
+            ),
+            'email'
+        )->message('This email already exists');
+
+        if ($v->validate()) {
+            echo 'All good';
+        } else {
+            throw new ValidationException($v->errors());
+            print_r($v->errors());
+        }
+
+        exit();
+
         $user = new User();
 
         $user->setName($data['name']);
         $user->setEmail($data['email']);
-        $user->setPassword(password_hash($data['password'], PASSWORD_BCRYPT,['cost'=>12]));
+        $user->setPassword(password_hash($data['password'], PASSWORD_BCRYPT, ['cost' => 12]));
         $this->entityManager->persist($user);
         $this->entityManager->flush();
         return $response;
